4cebb85e82d413fe8c1be677ee5e7f21f98430b2
Enoch Huang <48170726+ehuang3190@users.noreply.github.com>
2022-02-24 14:18:31 -0800

    changed graph formatting

--- report189.m
+++ report189.m
@@ -16,9 +16,9 @@ x = (sqrt(g./R)./((1+HM./R).^1.5)).*t;
 dFM = 1./(1+(1./c).*sqrt((g.*R)./(1+HM./R)).*sin(x)./sqrt((1+HM./R).^2-2.*(1+HM./R)*cos(x)+1))-1; L2 = '8062km (MEO)';
 plot(t, dFM, 'DisplayName','MEO');
 
-title("Doppler Shift of satellite sweeping over ground station");
-xlabel("Time (s)");
-ylabel("Doppler Shift (ppm)");
+title(["Doppler Shift of satellite", " sweeping over ground station"], 'FontSize', 18);
+xlabel("Time (s)", 'FontSize', 18);
+ylabel("Doppler Shift (ppm)", 'FontSize', 18);
 
 legend;
 drawnow;
